36d775584af24c21270cf19bbd8df98180228a4c
Eric Saxby <sax@livinginthepast.org>
2013-02-24 18:00:38 -0800

    Update description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'resource-control'
 maintainer       'Wanelo, Inc'
 maintainer_email 'ops@wanelo.com'
 license          'Apache 2.0'
-description      'Installs/Configures resource_control'
+description      'Lightweight resource providers for project and task management in Solaris'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
 
