d9785c1268a9524cb5155ffd7cffc7c5aa2c6bfc
Eric Saxby <sax@livinginthepast.org>
2013-05-04 10:57:56 -0700

    Fix project comment spec

--- spec/providers/project_spec.rb
+++ spec/providers/project_spec.rb
@@ -2,46 +2,42 @@ require 'spec_helper'
 
 describe 'resource-control::project' do
   before do
-    double_cmd('projects -l name-only')
+    double_cmd('projects -l project_name')
     double_cmd('projadd')
     double_cmd('projmod')
   end
 
   context 'project does not exist' do
-    before do
-      double_cmd('grep name-only /etc/project', exit: 1)
-    end
+    before { double_cmd('grep project_name /etc/project', exit: 1) }
 
     it "creates a project with only name attribute" do
       expect {
         converge_recipe %{
-            resource_control_project 'name-only'
+            resource_control_project 'project_name'
         }
-      }.to shellout('projadd -c "" name-only')
+      }.to shellout('projadd -c "" project_name')
     end
 
     it "sets a comment" do
       expect {
         converge_recipe %{
-            resource_control_project 'project' do
+            resource_control_project 'project_name' do
               comment "project comment"
             end
         }
-      }.to shellout('projadd -c "project comment" project')
+      }.to shellout('projadd -c "project comment" project_name')
     end
   end
 
   context 'project already exists' do
-    before do
-      double_cmd('grep name-only /etc/project', exit: 0)
-    end
+    before { double_cmd('grep project_name /etc/project', exit: 0) }
 
     it "creates a project with only name attribute" do
       expect {
         converge_recipe %{
-            resource_control_project 'name-only'
+            resource_control_project 'project_name'
         }
-      }.to shellout('projmod -c "" name-only')
+      }.to shellout('projmod -c "" project_name')
     end
   end
 end
